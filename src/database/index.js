5144d55d77003ed2c78dfb89d0e6ff2affc91b33
Matthew Gomez <magomez96@gmail.com>
2018-05-01 11:34:34 -0400

    Add a lot of DB functions
    Not tested yet

--- src/database/index.js
+++ src/database/index.js
@@ -138,6 +138,140 @@ const Database = function () {
         return result;
     };
 };
+
+this.insertUserIntoTable = async function (dbPool, tableName, user) {
+    if (!dbPool) {
+        throw 'Invalid dbPool Object';
+    }
+
+    if (!helper.isValidString(tableName)) {
+        throw 'Invalid Table Name for Inserting User';
+    }
+
+    if (!user) {
+        throw 'Invalid User Object to Insert into Database';
+    }
+
+    if (!helper.isValidString(user.id)) {
+        throw 'Invalid id for User to Insert into User Table';
+    }
+
+    if (!helper.isValidString(user.username)) {
+        throw 'Invalid username for User To Insert into User Table';
+    }
+
+    if (!helper.isValidStringIfExists(user.email)) {
+        throw 'Invalid email for User to Insert into User Table';
+    }
+    if (!helper.isValidStringIfExists(user.access_token)) {
+        throw 'Invalid access_token for User to Insert into User Table';
+    }
+    if (!helper.isValidStringIfExists(user.refresh_token)) {
+        throw 'Invalid refresh_token for User to Insert into User Table';
+    }
+    if (!helper.isValidStringIfExists(user.color_choice)) {
+        throw 'Invalid color_choice for User to Insert into User Table';
+    }
+
+    const insertIntoTable = 'INSERT INTO %I (' +
+        'id, username, email, access_token, refresh_token, color_choice) VALUES ' +
+        '($1, $2, $3, $4, $5, $6);';
+
+    const formattedQuery = format(
+        insertIntoTable,
+        tableName
+    );
+
+    const valueArray = [];
+
+    valueArray.push(user.id);
+    valueArray.push(user.username);
+    valueArray.push(user.email);
+    valueArray.push(user.access_token);
+    valueArray.push(user.refresh_token);
+    valueArray.push(user.color_choice);
+
+    const client = await dbPool.connect();
+    const result = await client.query(formattedQuery, valueArray);
+    client.release();
+
+    return result;
+};
+
+this.getUserById = async function (dbPool, tableName, id) {
+    if (!dbPool) {
+        throw 'Invalid dbPool Object';
+    }
+
+    if (!helper.isValidString(tableName)) {
+        throw 'Invalid Table Name for Getting ID';
+    }
+
+    if (!helper.isValidString(id)) {
+        throw 'Invalid ID to Look for User';
+    }
+
+    const selectQuery = 'SELECT * FROM %I WHERE id = %L;';
+
+    const client = await dbPool.connect();
+    const result = await client.query(format(selectQuery, tableName, id));
+    client.release();
+
+    return result;
+};
+
+this.getUserByUsername = async function (dbPool, tableName, username) {
+    if (!dbPool) {
+        throw 'Invalid database pool object - getting user info by username';
+    }
+    if (!helper.isValidString(tableName)) {
+        throw 'Invalid table name string - getting user info by username';
+    }
+    if (!helper.isValidString(username)) {
+        throw 'Invalid username string - getting user info by username';
+    }
+
+    const selectQuery = 'SELECT * FROM %I WHERE username = %L;';
+    let client;
+    let result;
+
+    try {
+        client = await dbPool.connect();
+        result = await client.query(format(selectQuery, tableName, usersname));
+    } catch (ex) {
+        console.error(ex);
+    } finally {
+        client.release();
+    }
+
+    return result;
+};
+
+this.getAllEmails = async function (dbPool, tableName) {
+    if (!dbPool) {
+        throw 'Non-existent or invalid database pool for getAllEmails';
+    }
+    if (!tableName || typeof tableName !== 'string' || tableName.length === 0) {
+        throw 'Non-existent or invalid user table name for getAllEmails';
+    }
+
+    const queryText = 'SELECT id, email FROM %I WHERE email<>"";';
+
+    let client;
+    let result;
+
+    try {
+        client = await dbPool.connect();
+        result = await client.query(format(queryText, tableName));
+    } catch (ex) {
+        console.error(ex);
+    } finally {
+        client.release();
+    }
+
+    return result;
+};
+
 const getDatabase = () => {
     let currentDatabase = null;
 
