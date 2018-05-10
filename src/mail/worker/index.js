5e20e0548c8cb40e3bb9d88d7aa82c658efac4a5
AlexMassenzio <alexmassenzio@gmail.com>
2018-05-09 20:36:00 -0400

    Removed sendgrid from worker.

--- src/mail/worker/index.js
+++ src/mail/worker/index.js
@@ -27,17 +27,4 @@ async function init()
 }
 
 
-// using SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey('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');
-const msg = {
-  to: 'test@example.com',
-  from: 'test@example.com',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-sgMail.send(msg);
-
 init();
