1773fa09520932b06fcec9d7965b7b07bf1f9f9b
Christian Harrypersad <psp1001k2006@gmail.com>
2018-05-10 23:07:03 -0400

    Comment styling pass

--- src/react/RedditMessage.js
+++ src/react/RedditMessage.js
@@ -36,6 +36,14 @@ class RedditMessage extends Component {
 		}
 	}
 
+	padComment = () => {
+		if (this.props.nest_level !== 0) {
+			return <div style={{
+				width: '1rem'
+			}} />
+		}
+	}
+
 	render() {
 
 		const getRepliesCount = () => {
@@ -58,7 +66,7 @@ class RedditMessage extends Component {
 			if (this.props.data.replies) {
 				if (!this.state.showReplies) {
 					return (
-						<button onClick={() => {this.setShowReplies(true)}} type="button" className="btn btn-primary">Show {getRepliesCount() === 1 ? '1 Reply' : `${getRepliesCount()} Replies`}</button>
+						<button onClick={() => {this.setShowReplies(true)}} type="button" className="btn btn-primary mb-3">Show {getRepliesCount() === 1 ? '1 Reply' : `${getRepliesCount()} Replies`}</button>
 					);
 				} else {
 					return (
@@ -66,7 +74,7 @@ class RedditMessage extends Component {
 							{ this.props.data.replies.data.children.map( (replies) => {
 								return <RedditMessage data={replies.data} key={replies.data.id} switchMainPage={this.props.switchMainPage} showReplies={true} kind={replies.kind} link_id={this.props.link_id} nest_level={this.props.nest_level+1}/>
 							})}
-							<button type="button" className="btn btn-danger" onClick={() => {this.setShowReplies(false);}}> Collapse Replies </button>
+							<button type="button" className="btn btn-danger mb-3" onClick={() => {this.setShowReplies(false);}}> Collapse Replies </button>
 						</div> 
 					);
 				}
@@ -123,7 +131,8 @@ class RedditMessage extends Component {
 				);
 			}
 			return (
-				<div className={`media pl-${this.props.nest_level}`}>
+				<div className="media">
+					{this.padComment()}
 					<div className="media-body">
 						<h5><Link to={'/RedditProfileDisplay/' + this.props.data.author}>u/{this.props.data.author}</Link> replied {helper.timeDifferenceString(new Date(this.props.data.created_utc * 1000), Date.now())} ago</h5>
 						<Linkify><Interweave tagName='fragment' transform={LinkTransform} content={this.props.data.body_html}/></Linkify>
